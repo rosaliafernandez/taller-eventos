c44adba46896c698e6f5d90bce71551ed12d4d21
Rosalía Fernández <rosaliafernandez138@gmail.com>
2025-08-15 20:44:44 -0300

    Solución al evento bubbling con stopPropagation

--- eventos.js
+++ eventos.js
@@ -1,5 +1,14 @@
+// Seleccionamos el botón y el contenedor
+const boton = document.querySelector("button");
 const contenedor = document.getElementById("contenedor");
 
+// Evento del botón: detiene la propagación del evento
+boton.addEventListener("click", function(event) {
+  event.stopPropagation(); // Evita que el evento llegue al div
+  alert("Hola!");
+});
+
+// Evento del div
 contenedor.addEventListener("click", function() {
   alert("Hola! Soy el div");
 });
